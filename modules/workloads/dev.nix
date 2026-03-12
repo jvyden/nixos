f282e595d78bbae160b91d264f518680bb0a00e7
jvyden <jvyden@jvyden.xyz>
2026-03-11 21:59:40 -0400

    Add some more dev tools

--- modules/workloads/dev.nix
+++ modules/workloads/dev.nix
@@ -2,10 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
+    # languages
+    gcc
+    clang
+    dotnetCorePackages.sdk_10_0
+    # tools, IDEs, debuggers
     angryipscanner
     vscodium
+    jetbrains.rider
     avalonia-ilspy
     imhex
-    jetbrains.rider
+    gdb
   ];
 }
