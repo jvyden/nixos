0fff3697101652696f9c55e2e49d4b4583451dea
jvyden <jvyden@jvyden.xyz>
2026-03-14 17:17:42 -0400

    Set spawn radius, whitelist/ops, increase view distance

--- machines/jvyden-pi5/minecraft/beta.nix
+++ machines/jvyden-pi5/minecraft/beta.nix
@@ -4,5 +4,42 @@
   services.minecraft-servers.servers.beta = {
     enable = true;
     package = self.packages.${pkgs.stdenv.hostPlatform.system}.uberbukkit;
+    serverProperties = {
+      view-distance = 16;
+      white-list = true;
+    };
+    files = {
+      "white-list.txt" = {
+        value = [
+          "jvyden420"
+          "lyristhekitori"
+        ];
+      };
+      "ops.txt" = {
+        value = [
+          "jvyden420"
+          "lyristhekitori"
+        ];
+      };
+      "bukkit.yml" = {
+        value = {
+          settings = {
+            update-folder = "update";
+            spawn-radius = 0;
+            permissions-file = "permissions.yml";
+          };
+          database = {
+            password = "walrus";
+            driver = "org.sqlite.JDBC";
+            isolation = "SERIALIZABLE";
+            url = "jdbc:sqlite:{DIR}{NAME}.db";
+            username = "bukkit";
+          };
+          aliases = {
+            icanhasbukkit = ["version"];
+          };
+        };
+      };
+    }
   };
 }
