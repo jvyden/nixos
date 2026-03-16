5abc31c5f22c297c6779f901230d3f9536fe63ff
jvyden <jvyden@jvyden.xyz>
2026-03-15 20:21:48 -0400

    Add minimal viaproxy configuration

--- machines/jvyden-pi5/minecraft/viaproxy.nix
+++ machines/jvyden-pi5/minecraft/viaproxy.nix
@@ -1,4 +1,40 @@
-{...}:
+{ self, pkgs, ... }:
+
+let
+  dataDir = "/var/lib/viaproxy";
+in
 {
-  
+  networking.firewall.allowedTCPPorts = [25565];
+
+  systemd.services.viaproxy = {
+    wantedBy = [ "multi-user.target" ];
+    description = "Standalone proxy which allows players to join EVERY Minecraft server version";
+    wants = [
+      "network-online.target"
+    ];
+    after = [
+      "network-online.target"
+    ];
+
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+
+      ProtectSystem = "strict";
+      ProtectHome = true;
+      ProtectKernelTunables = true;
+      ProtectKernelModules = true;
+      ProtectControlGroups = true;
+
+      DynamicUser = true;
+      PrivateTmp = true;
+      WorkingDirectory = dataDir;
+      StateDirectory = baseNameOf dataDir;
+      UMask = "0027";
+
+      ExecStart = "${self.packages.${pkgs.stdenv.hostPlatform.system}.viaproxy}/bin/viaproxy";
+    };
+  };
+
+  environment.systemPackages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.viaproxy ];
 }
