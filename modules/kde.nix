3942260d53133219398b0d6e2cf43c271a640209
jvyden <jvyden@jvyden.xyz>
2026-04-17 05:10:54 -0400

    failed attempt to get plasma-login-manager working

--- modules/kde.nix
+++ modules/kde.nix
@@ -2,7 +2,10 @@
 {
   services.auto-cpufreq.enable = lib.mkForce false;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.plasma-login-manager = {
-    enable = true;
-  };
+  services.displayManager.plasma-login-manager.enable = true;
+
+  environment.etc."plasmalogin.conf.d/98-bg.conf".text = ''
+    [Greeter][Wallpaper][org.kde.image][General]
+    Image=file://${../assets/wallpapers/bg2.jpg}
+  '';
 }
