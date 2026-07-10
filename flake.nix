f2b449254bd9bb1b0cb551e7e2777937882381c0
jvyden <jvyden@jvyden.xyz>
2026-07-10 14:57:38 -0400

    Update continuwuity

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
-    continuwuity.url = "git+https://forgejo.ellis.link/continuwuation/continuwuity?ref=main&rev=dfadc4575517366e1773b005b953b27cf0af710f";
+    continuwuity.url = "git+https://forgejo.ellis.link/continuwuation/continuwuity?ref=main&rev=5e306f160a61390565f09ad6056128e7cdcc490f";
     copyparty.url = "github:9001/copyparty";
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     agenix = {
