c1c7e9b473208e77b0921500797de15da62389ed
jvyden <jvyden@jvyden.xyz>
2026-09-08 21:10:53 -0400

    feat: Add WiFi route to jvyden-pi5

--- machines/jvyden-pi5/machine.nix
+++ machines/jvyden-pi5/machine.nix
@@ -19,12 +19,21 @@
   networking = {
     hostName = "jvyden-pi5";
     defaultGateway = "10.0.0.1";
-    interfaces.end0.ipv4.addresses = [
-      {
-        address = "10.0.0.228";
-        prefixLength = 24;
-      }
-    ];
+    interfaces.end0.ipv4 = {
+      addresses = [
+        {
+          address = "10.0.0.228";
+          prefixLength = 24;
+        }
+      ];
+      routes = [
+        {
+          address = "10.1.0.0";
+          prefixLength = 24;
+          via = "10.0.0.1";
+        }
+      ];
+    };
   };
 
   # apply some extra features since we're both constrained on memory and constrained on how much we should be writing
